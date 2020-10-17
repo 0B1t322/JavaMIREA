cd5a26f14afa0e16164005f5fbce2e79462a8243
0B1t322 <dan.demin@gmail.com>
2020-10-17 20:59:17 +0300

    Add task_3

---
+++ src/ru/demin/pract12/task_3.java
@@ -0,0 +1,105 @@
+package ru.demin.pract12;
+
+import java.text.Format;
+import java.util.*;
+
+
+public class task_3 {
+    final static int LENGHT = 11;
+
+    final static String format = "+7 (%s%s%s) %s%s%s-%s%s-%s%s";
+
+    public static void main(String[] args) {
+        Formatter formatter = new Formatter();
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> nums = new ArrayList<String>(Arrays.asList(
+                scanner.nextLine().replace("+7","7").
+                replace(" ", "").
+                replace("-","").
+                replace("(", "").
+                replace(")", "").
+                split("")));
+
+        switch (nums.size()) {
+            case 11:
+                if(!nums.get(0).equals("8") && !nums.get(0).equals("7")) {
+                    System.out.println("Неверный формат номера");
+                    return;
+                } else if(nums.get(0).equals("8")){
+                    nums.remove("8");
+                    System.out.println(formatPhoneNumber(nums));
+                } else {
+                    nums.remove("7");
+                    System.out.println(formatPhoneNumber(nums));
+                }
+                break;
+            case 10:
+                System.out.println(formatPhoneNumber(nums));
+                break;
+            default:
+                System.out.println("Неверный формат номера");
+                break;
+        }
+    }
+
+    public static String formatPhoneNumber(List<String> nums) {
+        String phoneNumber = "+7 (";
+        /*
+        int i = 0;
+        for( ;i < 3;i++) {
+            phoneNumber += nums.get(i);
+        }
+        phoneNumber += " ";
+        for (; i < 6; i++) {
+            phoneNumber += nums.get(i);
+        }
+        phoneNumber += " ";
+
+        for(; i < 8; i++) {
+            phoneNumber += nums.get(i);
+        }
+
+        phoneNumber += " ";
+
+        for(; i < 10; i++) {
+            phoneNumber += nums.get(i);
+        }*/
+        int i = 0;
+        while (i < nums.size()){
+            int count = 0;
+            for(int k = 0; k < 2 ; k++) {
+                for (int j = 0; j < 3; j++) {
+
+                    phoneNumber += nums.get(i);
+                    count++;
+                    i++;
+                    if (count == 3) {
+                        if(k == 0) phoneNumber += ") ";
+                        else phoneNumber += "-";
+                        count = 0;
+                    }
+                }
+            }
+
+            count = 0;
+            for(int k = 0; k < 2; k++) {
+                for (int j = 0; j < 2; j++) {
+
+                    phoneNumber += nums.get(i);
+                    count++;
+                    i++;
+                    if (count == 2) {
+                        if(k != 1)phoneNumber += "-";
+                        count = 0;
+                    }
+                }
+            }
+            return phoneNumber;
+        }
+
+        return phoneNumber;
+    }
+
+
+}
